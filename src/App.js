e0b696ffddb4869b54dbb503fd3410f45cec4f95
FreezeByte <rosas.ruizlg@gmail.com>
2020-03-30 23:11:11 -0600

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
 
@@ -42,9 +43,16 @@ function App() {
       
     }
     consultarAPI();
+    // eslint-disable-next-line
   }, [consultar]);
 
- 
+  let componente;
+      if(error){
+        componente = <Error mensaje="No hay resultados" />
+      }else{ 
+          componente = <Clima resultado={resultado} />
+      }
+  
 
   return (
     <Fragment>
@@ -62,9 +70,7 @@ function App() {
                 />
               </div>
               <div className="col m6 s12">
-                <Clima
-                    resultado={resultado}
-                />
+                {componente}
               </div>
             </div>
         </div>
